3d490350aeeb29cae6660451eb4a5961e924cd77
Trey <trey.husko@gmail.com>
2021-04-30 18:30:19 -0700

    Added 2 tests for each calc, 4 pass/4 fail

--- calc.py
+++ calc.py
@@ -8,8 +8,11 @@ def multiply(a,b):
     mult = a * b
     return mult
 def divide(a,b):
+    if b == 0:
+        return ZeroDivisionError
     div = a / b
     return div
 def calc():
     numA = int(input("enter a num"))
     num = int(input("Enter a 2nd Number: "))
+    
--- test_calc.py
+++ test_calc.py
@@ -8,5 +8,28 @@ class testCaseAdd(unittest.TestCase):
     def test_sum2(self):
         result = calc.sum(5,5)
         self.assertEqual(result, 11)
+
+class testCaseSub(unittest.TestCase):
+    def test_diff(self):
+        result = calc.diff(5,10)
+        self.assertEqual(result, -5)
+    def test_diff2(self):
+        result = calc.diff(10, 5)
+        self.assertEqual(result, 6)
+
+class testCaseMult(unittest.TestCase):
+    def test_multiply(self):
+        result = calc.diff(5,10)
+        self.assertEqual(result, -5)
+    def test_multiply2(self):
+        self.assertRaises(TypeError, calc.multiply, 1, 'two')
+
+class testCaseDiv(unittest.TestCase):
+    def test_divide(self):
+        result = calc.diff(5,10)
+        self.assertEqual(result, -5)
+    def test_divide2(self):
+        self.assertRaises(ZeroDivisionError, calc.divide, 5, 0)
+        
 if __name__ == "__main__":
     unittest.main()
